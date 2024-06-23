2351a1798d09846905bd06aa76ddd738db078e11
Sandesh Poudel <141824006+Sandesh-007@users.noreply.github.com>
2024-06-23 21:00:34 +0545

    Lab02_04.cpp

--- Lab02_04.cpp
+++ Lab02_04.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Function to return a reference to the larger of two double variables
 double& larger(double &a, double &b) {
     return (a > b) ? a : b;
 }
@@ -15,11 +16,14 @@ int main() {
     cout << "Enter temperature 2: ";
     cin >> temp2;
 
-    // Prompt the user to enter the new temperature, and assign it to the larger variable
-    cout << "Enter new temperature to replace the larger one: ";
-    cin >> larger(temp1, temp2);
+    // Declare a reference variable to the larger of the two temperatures
+    double &largerTemp = larger(temp1, temp2);
 
-    // Output the results
+    // Prompt the user to enter a new temperature to replace the larger one
+    cout << "Enter new temperature: ";
+    cin >> largerTemp;
+
+    // Output the resulting temperatures
     cout << "Temp1: " << temp1 << ", Temp2: " << temp2 << endl;
 
     return 0;
